51554878934e05eabd66071808a25730e2a8e804
Seongbae94 <seongbaelee944@gmail.com>
2023-01-04 10:46:44 +0900

    feat: 그룹 생성 모달 추가

---
+++ src/components/GroupModal.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const styles = {
+  OVERLAY_STYLE: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0, .7)',
+    zIndex: 1000,
+  } as React.CSSProperties,
+
+  MODAL_STYLES: {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    // backgroundColor: 'white',
+    padding: '50px',
+    zIndex: 1000,
+  } as React.CSSProperties,
+};
+
+interface Props {
+  open?: boolean;
+  onClose?: () => void;
+  children?: React.ReactNode;
+}
+
+export const Modal: React.FC<Props> = ({ open, children, onClose }) => {
+  if (!open) return null;
+  return ReactDOM.createPortal(
+    <div>
+      <div style={styles.OVERLAY_STYLE} />
+      <div style={styles.MODAL_STYLES}>
+        <div>{children}</div>
+        {/* <button onClick={onClose}>Close Modal</button> */}
+      </div>
+    </div>,
+    document.getElementById('portal')!,
+  );
+};
