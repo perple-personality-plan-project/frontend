45451aa1db9af81b0137d23ccce96ab602af61ad
Seongbae94 <seongbaelee944@gmail.com>
2023-01-05 16:54:44 +0900

    feat: 루트 수정

--- src/shared/Routes.js
+++ src/shared/Routes.js
@@ -1,11 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GroupPage from '../pages/GroupPage';
+import GroupDetail from '../pages/GroupDetail';
+import MbtiQuestionsPage from '../pages/MbtiQuestionsPage';
+import NaviBar from '../components/NaviBar';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<GroupPage />} />
+        <Route path="/group" element={<GroupPage />} />
+        <Route path="/:id" element={<GroupDetail />} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <NaviBar />
+              <GroupPage />
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
