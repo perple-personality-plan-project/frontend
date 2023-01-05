b9c8e58ab19106fa3b19f9af6511b79e47f7a6a3
Seongbae94 <seongbaelee944@gmail.com>
2023-01-05 16:49:40 +0900

    feat: 그룹 디테일 페이지 생성

---
+++ src/pages/GroupDetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router';
+
+const GroupDetail = () => {
+  const id = useParams();
+  return <div>GroupDetail</div>;
+};
+
+export default GroupDetail;
