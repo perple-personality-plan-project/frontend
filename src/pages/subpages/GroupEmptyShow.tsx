3add2ce97063eaae7875f56b67d9ee35e891ee61
Seongbae94 <seongbaelee944@gmail.com>
2023-01-21 01:02:55 +0900

    update: 그룹 페이지 업데이트
    인증된 회원만 그룹 작성하기 가능.
    태그 서버에서 불러오게 함.
    그룹 한개도 없을 때 그룹 없는 페이지 보여주게 함.

---
+++ src/pages/subpages/GroupEmptyShow.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+import styled from 'styled-components';
+
+const GroupEmptyShow = () => {
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem('accessToken');
+  console.log(accessToken);
+
+  return (
+    <div>
+      {accessToken === null ? (
+        <StContainer>
+          <p>로그인 후 그룹을 만들어주세요!</p>
+          <div onClick={() => navigate('/signin')}>
+            <p className="login">로그인 하러 가기</p>
+            <p className="arrow">{`->`}</p>
+          </div>
+        </StContainer>
+      ) : (
+        <StContainer>
+          <p>우측 하단 버튼을 클릭해 그룹을 만들어주세요!</p>
+        </StContainer>
+      )}
+    </div>
+  );
+};
+
+export default GroupEmptyShow;
+
+const StContainer = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+
+  p {
+    margin: 0 0 10px 0;
+  }
+
+  div {
+    font-size: 15px;
+    display: flex;
+    cursor: pointer;
+    &:hover {
+      .arrow {
+        padding-left: 5px;
+      }
+    }
+  }
+`;
