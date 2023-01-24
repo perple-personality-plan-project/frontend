b22ba8959df676233771bfdc0c296afdf18749e6
Seongbae94 <seongbaelee944@gmail.com>
2023-01-24 18:29:57 +0900

    style: 메인 페이지 스타일 수정
    메인 페이지 스타일 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -258,7 +258,7 @@ const MbtiTitle = styled.div`
 const PostListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin-top: 20vh;
+  margin-top: 15vh;
   justify-content: center;
   gap: 25px;
   border: 5px soild red;
